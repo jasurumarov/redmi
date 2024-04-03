b8998bc3fe223a8b8a59042575d1c63c4eeedbe3
jasurumarov <jasurumarov199@gmail.com>
2024-04-04 01:51:15 +0500

    finished

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -105,7 +105,7 @@ function Footer() {
                     </div>
                     <h3 className="footer-top__form__heading">Let's stay in touch</h3>
                     <label className='footer-top__form-box'>
-                        <input className='footer-top__form-box__input' type="email" placeholder='Enter email adress'/>
+                        <input required className='footer-top__form-box__input' type="email" placeholder='Enter email adress'/>
                         <button>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="20px" fill='white' viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
                         </button>
